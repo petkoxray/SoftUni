5454e9db3d4af79e1da96bdfd58abdbf23a8af09
petkoxray <petkoxray@gmail.com>
2017-01-17 11:09:24 +0200

    Arrays and Lists homework in process

---
+++ Programming Fundamentals Homeworks/ArraysAndLists-Exercices/01.LargestCommonEnd/01.LargestCommonEnd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var firstArray = Console.ReadLine().Split();
+        var secondArray = Console.ReadLine().Split();
+
+        int minLen = Math.Min(firstArray.Length, secondArray.Length);
+        int rightCount = 0;
+        int leftCount = 0;
+
+        for (int i = 0; i < minLen; i++)
+        {
+            if (firstArray[i] == secondArray[i])
+            {
+                leftCount++;
+            }
+        }
+
+        for (int i = 1; i <= minLen; i++)
+        {
+            if (firstArray[firstArray.Length - i] == secondArray[secondArray.Length - i])
+            {
+                rightCount++;
+            }
+        }
+
+        Console.WriteLine(Math.Max(rightCount,leftCount));
+    }
+}
+
